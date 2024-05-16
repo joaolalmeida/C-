1f1fb5ddfd4818dac968655706f63621c2b62412
joaolalmeida <joaolalmeida20@gmail.com>
2024-05-16 17:40:54 -0300

    declaration of variables and types

--- Aula 01/Program.cs
+++ Aula 01/Program.cs
@@ -7,7 +7,26 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World!\n"); //Pular linha \n
+            /* Int - -156 como 32334
+               Float - -15,6 como 232.232
+               Bool - true ou false
+               String - "dasda dada"
+               Char - 'a'
+                                     */
+            int segundaGuerraMundial = 1942;
+            string cor_favorita = "Vermelho";
+            float velocidadeF1 = 294.23f;
+            bool segundaGuerraMundialAconteceu = true;
+
+            Console.WriteLine(segundaGuerraMundial);
+            Console.WriteLine(cor_favorita);
+            Console.WriteLine(velocidadeF1);
+
+            velocidadeF1 = 348.29f;
+
+            Console.WriteLine(velocidadeF1);
+
+            Console.WriteLine("Hello World!\n"); //Pular linha \n e tambem o WriteLine
             Console.WriteLine("Meu nome é Joao Almeida\n");
 
             Console.ReadLine();
